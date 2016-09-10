cd3d40c463058acd139a916d3eeab59835d55303
Paul Gundarapu <leogps@gmail.com>
2016-09-09 23:28:44 -0700

    Removing redundant files.

--- run_java_1.6.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home/bin/java -jar ./player-ui/target/ItunesMediaPlayer-jar-with-dependencies.jar
--- run_java_latest.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-java -jar ./player-ui/target/ItunesMediaPlayer-jar-with-dependencies.jar
